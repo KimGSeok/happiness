fd2d50c668657cf9c3acf9ef662c8fb514bf904e
KimGSeok <rla99tjr@gmail.com>
2024-08-27 18:47:05 +0900

    [HPFE-8] refactor: History 컴포넌트 노출방식 변경

--- src/features/introduce/screen/Introduce.tsx
+++ src/features/introduce/screen/Introduce.tsx
@@ -1,7 +1,9 @@
+import { css } from "styled-system/css";
+
 const IntroduceScreen = () => {
   return (
-    <div>
-      <h1>Introduce</h1>
+    <div className={css({ padding: '16px 0' })}>
+      <h1>페이지를 준비중이에요.</h1>
     </div>
   );
 }
